8c170e33902c3a27a2df1cfe99ca8825b5debdba
Ben Paget <ben.paget@live.fr>
2021-04-07 20:49:16 +0100

    Reboot workshop intro

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from "react";
-import OneLevelDown from './OneLevelDown';
+import React, { useState } from "react";
+import OneLevelDown from "./OneLevelDown";
+import UserContext from "./contexts/UserContext"
 
 const App = () => {
-  const loggedInUser = {
+
+  const [ userData, setUserData ] = useState({
     username: "Ben",
     email: "ben@wbs.com",
-    isAdmin: true,
-  };
+    isAdmin: true
+  })
 
   return (
     <>
-  <h1>
-  Context Workshop
-  </h1>
-  <OneLevelDown/>
-  </>
+      <h1>Context Workshop</h1>
+      <UserContext.Provider value={{userData, setUserData}}>
+        <OneLevelDown />
+      </UserContext.Provider>
+    </>
   );
-}
+};
 
 export default App;
--- src/ThreeLevelsDown.js
+++ src/ThreeLevelsDown.js
@@ -1,10 +1,13 @@
 import React from "react"
+import Logout from "./Logout"
 
 const ThreeLevelsDown = () => {
+
     return (
         <div>
             <p>Three Levels Down! ⬇️</p>
             <h3>🤷 User info should be here! 🤷</h3>
+            <Logout />
         </div>
     )
 }
